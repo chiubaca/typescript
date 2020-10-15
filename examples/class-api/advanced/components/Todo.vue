a1c6c86d0a670b2b4c93ae55ed49aa1a53f4ca7b
Alex Chiu <alexchiu11@gmail.com>
2020-10-15 21:06:03 +0100

    Initial implementation of todo app

---
+++ examples/class-api/advanced/components/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="to-do">
+    <span :class="{complete: task.complete}">{{ task.name }}</span>
+    <button @click="$emit('complete', index)">
+      ✅
+    </button>
+    <button @click="$emit('delete', index)">
+      🗑️
+    </button>
+  </li>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Task } from '../types'
+
+@Component
+export default class Todo extends Vue {
+@Prop() readonly task!: Task;
+@Prop() readonly index!: Number;
+}
+</script>
+
+<style >
+.complete{
+  text-decoration: line-through
+}
+</style>
